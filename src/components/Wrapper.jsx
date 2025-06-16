a54d455e508f1c2ce33209cee92dd23b70ff176c
gift-y <giftydomi72@gmail.com>
2025-06-16 14:56:33 +0000

    added new section

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Wrapper = ({ children }) => {
   return (
-    <div className='max-w-5xl mx-auto px-6 flex items-center justify-between'>{children}</div>
+    <div className='max-w-5xl md:px-6max-w-5xl mx-auto px-6 flex items-center justify-center'>{children}</div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,97 @@
-import React from 'react'
-import Wrapper from '../components/Wrapper';
-import Button from '../components/Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Wrapper from "../components/Wrapper";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
+import Features from "../components/Features";
+import { content } from "../utils/Data";
 
 const Home = () => {
   return (
-   <Wrapper className="">
-       <section>
-         <div className="flex items-center justify-center flex-col gap-6 min-h-[100dvh] w-full">
-           <h1 className="text-4xl md:text-8xl md:leading-22 tracking-tight font-semibold max-w-5xl text-center text-[var(--secondary-color)] ">
-             Personalized Education, Driven by AI
-           </h1>
-           <p className="text-base md:text-2xl leading-7 max-w-xs md:max-w-2xl text-center md:py-6  text-[var(--secondary-color)]">
-             Empower every learner with AI-crafted education, unlocking
-             personalized paths to success and growth.
-           </p>
-           <div className="hidden md:flex items-center justify-center gap-5">
-             <Button size="large" variant="primary">
-               <Link to="/get-started">Get Started</Link>
-             </Button>
-             <Button size="large" variant="secondary">
-               <Link to="/about">Learn More</Link>
-             </Button>
-           </div>
-           <div className="md:hidden flex items-center justify-center gap-3">
-             <Button size="small" variant="primary">
-               <Link to="/get-started">Get Started</Link>
-             </Button>
-             <Button size="small" variant="secondary">
-               <Link to="/about">Learn More</Link>
-             </Button>
-           </div>
-         </div>
-       </section>
-       </Wrapper>
- 
-      
-  )
-}
+    <>
+      <Wrapper className="">
+        <section className="">
+          <div className="flex items-center justify-center flex-col gap-6 min-h-[100dvh] w-full">
+            <h1 className="text-4xl md:text-8xl md:leading-22 tracking-tight font-semibold max-w-5xl text-center text-[var(--secondary-color)] ">
+              Personalized Education, Driven by AI
+            </h1>
+            <p className="text-base md:text-2xl leading-7 max-w-xs md:max-w-2xl text-center md:py-6  text-[var(--secondary-color)]">
+              Empower every learner with AI-crafted education, unlocking
+              personalized paths to success and growth.
+            </p>
+            <div className="hidden md:flex items-center justify-center gap-5">
+              <Button size="large" variant="primary">
+                <Link to="/get-started">Get Started</Link>
+              </Button>
+              <Button size="large" variant="secondary">
+                <Link to="/about">Learn More</Link>
+              </Button>
+            </div>
+            <div className="md:hidden flex items-center justify-center gap-3">
+              <Button size="small" variant="primary">
+                <Link to="/get-started">Get Started</Link>
+              </Button>
+              <Button size="small" variant="secondary">
+                <Link to="/about">Learn More</Link>
+              </Button>
+            </div>
+          </div>
+        </section>
+      </Wrapper>
 
-export default Home
+      <Wrapper>
+        <section className="mt-24 px-4 w-full">
+          <div className="max-w-6xl mx-auto w-full rounded-2xl overflow-hidden bg-white shadow-md">
+            <img
+              src="#"
+              alt="App Interface Preview"
+              className="w-full h-[28rem] object-cover"
+            />
+            <div className="py-6 text-center">
+              <h2 className="text-[var(--primary-color)] font-bold text-3xl md:text-4xl">
+                Image of the interface
+              </h2>
+            </div>
+          </div>
+
+          <div className="mt-20 md:mt-32 px-4">
+            <h2 className="text-3xl lg:text-6xl text-center font-semibold text-[var(--secondary-color)] leading-tight">
+              Involve your audience with dynamic, interactive quizzes.
+            </h2>
+          </div>
+        </section>
+      </Wrapper>
+
+      <Wrapper>
+        <Features />
+      </Wrapper>
+
+      <Wrapper>
+        <div  className="flex flex-col items-start justify-center w-full">
+          <h2 className="text-3xl text-[var(--secondary-color)] md:text-5xl w-full md:w-xl text-left font-semibold pt-20 md:pt-20 lg:pt-40 py-10">
+            Designed for student groups, study sessions, and collaborative
+            projects.
+          </h2>
+
+          <div className="flex flex-col md:flex-row w-full gap-4 h-[80dvh]">
+            {content.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex flex-col w-full md:w-1/3 h- p-5 bg-[var(--accent)] rounded-xl"
+                >
+                  <h4 className="md:text-2xl text-[var(--secondary-color)] text-3xl font-bold py-6">
+                    {item.title}
+                  </h4>
+                  <p className="text-[var(--secondary-color)]">{item.description}</p>
+                  <div className={`w-full h-[200px] ${item.bgColor}`}></div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Home;
