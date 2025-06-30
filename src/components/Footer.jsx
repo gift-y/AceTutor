37ae8ce5a926400f1657a1c92f525dece85262b8
joshuaddf <jkdodofoli@gmail.com>
2025-06-30 17:17:19 +0000

    Fixed the content section and added the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import Wrapper from './Wrapper'
 const Footer = () => {
   return (
     <footer className='bg-[var(--secondary-color)] py-6 mt-10 h-2/3 w-full'>
-        <Wrapper>
-            <p className='text-white'>This is the footer</p>
-        </Wrapper>
+      <Wrapper>
+        <p className='text-white'>This is the footer</p>
+      </Wrapper>
     </footer>
   )
 }
